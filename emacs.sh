eeede6e6680acd5185df6a23c842e78dfc251cf1
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2023-05-11 16:11:46 -0300

    Atualizações

---
+++ emacs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Parâmetros
+DIR=$HOME/.emacs.d
+
+# Faz o pull do repositório do Emacs
+cd $DIR
+git pull
+cd $HOME
+
+# Inicia Emacs:
+/opt/emacs/bin/emacs
+
+# Faz push para repositório do Emacs
+cd $DIR
+git add .
+git commit -m "Atualizações"
+git push
