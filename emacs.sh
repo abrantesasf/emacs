c8324eb1b0e3ee8ffcfed13b1c2d48c87e44ed86
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2023-05-11 21:56:28 -0300

    Atualizações

--- emacs.sh
+++ emacs.sh
@@ -14,5 +14,7 @@ cd $HOME
 # Faz push para repositório do Emacs:
 cd $DIR
 git add .
+su - -c 'update-alternatives --set pinentry $(update-alternatives --list pinentry | grep pinentry-gnome3)'
 git commit -m "Atualizações"
 git push
+su - -c 'update-alternatives --set pinentry $(update-alternatives --list pinentry | grep pinentry-tty)'
