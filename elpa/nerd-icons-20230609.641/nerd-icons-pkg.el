20e66965b0479d01a2e68094b8d2117f63f62761
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2023-06-09 22:43:56 -0300

    Atualizações

--- elpa/nerd-icons-20230609.641/nerd-icons-pkg.el
+++ elpa/nerd-icons-20230609.641/nerd-icons-pkg.el
@@ -1,6 +1,6 @@
-(define-package "nerd-icons" "20230603.1813" "Emacs Nerd Font Icons Library"
+(define-package "nerd-icons" "20230609.641" "Emacs Nerd Font Icons Library"
   '((emacs "24.3"))
-  :commit "f7536c5fb9d61da091cb510bdbb545a7b7ea7480" :authors
+  :commit "a6999ddd8b85043bb24547611524a7ec86c6a21f" :authors
   '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
   :maintainers
   '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
