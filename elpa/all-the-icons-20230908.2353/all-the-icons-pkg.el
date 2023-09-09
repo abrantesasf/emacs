9c3b4c4ead91cead56ae4edd9617d219b08c3d45
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2023-09-09 01:15:02 -0300

    Atualizações

---
+++ elpa/all-the-icons-20230908.2353/all-the-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "all-the-icons" "20230908.2353" "A library for inserting Developer icons"
+  '((emacs "24.3"))
+  :commit "6ceb25dc1875bac5f595a935a1e104c78e131242" :authors
+  '(("Dominic Charlesworth" . "dgc336@gmail.com"))
+  :maintainers
+  '(("Dominic Charlesworth" . "dgc336@gmail.com"))
+  :maintainer
+  '("Dominic Charlesworth" . "dgc336@gmail.com")
+  :keywords
+  '("convenient" "lisp")
+  :url "https://github.com/domtronn/all-the-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
