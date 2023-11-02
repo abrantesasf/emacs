10e77d17676379a2fbf1fb18a7393060d67ae071
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2023-11-01 22:26:01 -0300

    Atualizações

--- elpa/transient-20231031.1300/transient-pkg.el
+++ elpa/transient-20231031.1300/transient-pkg.el
@@ -1,8 +1,8 @@
-(define-package "transient" "20231030.1943" "Transient commands"
+(define-package "transient" "20231031.1300" "Transient commands"
   '((emacs "26.1")
     (compat "29.1.4.1")
     (seq "2.24"))
-  :commit "92572454b5f022a0c05f1de590987168bbf63242" :authors
+  :commit "2eb214a6ecbd632760706de9056441fb581ec5fa" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainers
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
