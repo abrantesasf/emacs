65332b69452009b7589213a6770a080fd3e9322a
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2023-11-12 00:20:19 -0300

    Atualizações

--- elpa/company-20231110.52/company-pkg.el
+++ elpa/company-20231110.52/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20231023.1033" "Modular text completion framework"
+(define-package "company" "20231110.52" "Modular text completion framework"
   '((emacs "25.1"))
-  :commit "66201465a962ac003f320a1df612641b2b276ab5" :authors
+  :commit "07d1c41f83d5ad5f6263e726283a05ad12cd37a5" :authors
   '(("Nikolaj Schumacher"))
   :maintainers
   '(("Dmitry Gutov" . "dmitry@gutov.dev"))
--- elpa/slime-20231111.1312/slime-pkg.el
+++ elpa/slime-20231111.1312/slime-pkg.el
@@ -1,7 +1,7 @@
-(define-package "slime" "20231004.1552" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20231111.1312" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
-  :commit "0cc2e736112a0bc2a048ef6efd11dd67e3fbf7ad" :keywords
+  :commit "875f14f593da850c3641b22b688fec2f24c511c2" :keywords
   '("languages" "lisp" "slime")
   :url "https://github.com/slime/slime")
 ;; Local Variables:
