6411887fd81007a728b47bca47a6b423c8a8446f
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2023-12-01 02:05:18 -0300

    Atualizações

--- elpa/slime-20231130.1102/slime-pkg.el
+++ elpa/slime-20231130.1102/slime-pkg.el
@@ -1,7 +1,7 @@
-(define-package "slime" "20231127.953" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "20231130.1102" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
-  :commit "c753c7e912d42debd2c43c03f3205996bb1097ae" :keywords
+  :commit "a924516084ad7da57617ebc19086ebe7b6562ec8" :keywords
   '("languages" "lisp" "slime")
   :url "https://github.com/slime/slime")
 ;; Local Variables:
--- elpa/transient-20231129.1714/transient-pkg.el
+++ elpa/transient-20231129.1714/transient-pkg.el
@@ -1,8 +1,8 @@
-(define-package "transient" "20231128.1536" "Transient commands"
+(define-package "transient" "20231129.1714" "Transient commands"
   '((emacs "26.1")
     (compat "29.1.4.4")
     (seq "2.24"))
-  :commit "9050a0d058041e5e54d4e16359fd061e779c2ecb" :authors
+  :commit "5f2cfc9f73e756fb337df70909fcd3d0bce56911" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainers
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
