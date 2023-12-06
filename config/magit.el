627ae792bfd9b332cec3ecfa0e5616c58b6bec4b
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2023-12-06 12:27:32 -0300

    Atualizações

--- config/magit.el
+++ config/magit.el
@@ -1,7 +1,10 @@
 ;;----------------------------------------------------------;;
 ;; Configurações para uso do Git com GPG e Magit
 ;;----------------------------------------------------------;;
-;; https://emacs.stackexchange.com/questions/64578/emacs-pinentry-not-working-on-emacs-28-0-50-and-ubuntu-20-04
+;; https://emacs.stackexchange.com/questions/64579/emacs-pinentry-not-working-on-emacs-28-0-50-and-ubuntu-20-04
+;; https://emacs.stackexchange.com/questions/32881/enabling-minibuffer-pinentry-with-emacs-25-and-gnupg-2-1-on-ubuntu-xenial/68304#68304
 
-(setq epa-pinentry-mode 'loopback)
-(pinentry-start)
+;; In ~/.gnupg/gpg-agent.conf:
+;; allow-emacs-pinentry
+(require 'epg)
+(setq epg-pinentry-mode 'loopback)
