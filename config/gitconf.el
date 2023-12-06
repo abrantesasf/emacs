aa1536a061d0c06cba2fa363039bb564be9cbbbe
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2023-12-06 17:34:38 -0300

    Atualizações

--- config/gitconf.el
+++ config/gitconf.el
@@ -1,14 +1,19 @@
 ;;----------------------------------------------------------;;
 ;; Configurações para uso do Git com GPG e Magit
 ;;----------------------------------------------------------;;
-;; https://emacs.stackexchange.com/questions/64579/emacs-pinentry-not-working-on-emacs-28-0-50-and-ubuntu-20-04
-;; https://emacs.stackexchange.com/questions/32881/enabling-minibuffer-pinentry-with-emacs-25-and-gnupg-2-1-on-ubuntu-xenial/68304#68304
+;; Fazer a combinação de "Emacs + GnuPG + Magit + Git" funcionar corretamente
+;; foi um parto. Nada funcionava direito, trocentas mensagens de erro
+;; diferentes, sites e mais sites na internet sugerindo soluções as mais
+;; diversas possíveis e nenhuma dava certo. Consegui chegar na solução
+;; abaixo, que está funcionando, mas só Jesus sabe porque funciona...
+;;
+;; Colocar em "~/.gnupg/gpg-agent.conf" e fazer o reload do agent
+;; com "gpgconf --kill gpg-agent":
+;;     allow-emacs-pinentry
+;;     allow-loopback-pinentry
+;;     pinentry-program /usr/bin/pinentry-gnome3
 
-;; In ~/.gnupg/gpg-agent.conf:
-;; allow-emacs-pinentry
 (require 'epg)
 (setq epg-gpg-program "gpg")
 (setenv "GPG_AGENT_INFO" nil)
 (setq epa-pinentry-mode 'loopback)
-;;(pinentry-start)
-
