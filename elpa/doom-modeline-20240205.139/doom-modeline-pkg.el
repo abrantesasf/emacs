f376e0bb188c4c7f6618f58232a1b467da9b3adf
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-02-05 11:32:32 -0300

    Atualizações

--- elpa/doom-modeline-20240205.139/doom-modeline-pkg.el
+++ elpa/doom-modeline-20240205.139/doom-modeline-pkg.el
@@ -1,9 +1,9 @@
-(define-package "doom-modeline" "20240112.1056" "A minimal and modern mode-line"
+(define-package "doom-modeline" "20240205.139" "A minimal and modern mode-line"
   '((emacs "25.1")
     (compat "29.1.4.2")
     (nerd-icons "0.1.0")
     (shrink-path "0.3.1"))
-  :commit "a43bc7a222e054a4c3c0b97028f1dec1c596c114" :authors
+  :commit "9816e6a9d1216daf8f1ad07a04e8e657e3c6656a" :authors
   '(("Vincent Zhang" . "seagle0128@gmail.com"))
   :maintainers
   '(("Vincent Zhang" . "seagle0128@gmail.com"))
