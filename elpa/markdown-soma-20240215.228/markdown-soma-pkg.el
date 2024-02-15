dafb85a07ff59b9f9b3a89b8d3730d63eeb669c5
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-02-15 11:15:08 -0300

    Atualizações

---
+++ elpa/markdown-soma-20240215.228/markdown-soma-pkg.el
@@ -0,0 +1,17 @@
+(define-package "markdown-soma" "20240215.228" "Live preview for Markdown"
+  '((emacs "25")
+    (s "1.11.0")
+    (dash "2.19.1")
+    (f "0.20.0"))
+  :commit "ba30e609108d32fe6e1998490548b4631e3e48c3" :authors
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainers
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :keywords
+  '("wp" "docs" "text" "markdown")
+  :url "https://github.com/jasonm23/markdown-soma")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
