c376c98b3f602425fde158637b1454cef4079ffd
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2024-03-12 11:28:28 -0300

    Atualizações

--- elpa/slime-20240304.1122/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "slime" "20240304.1122" "Superior Lisp Interaction Mode for Emacs"
-  '((emacs "24.3")
-    (macrostep "0.9"))
-  :commit "f68ac1235a3eec3a7e337bd31443b9753ed8a850" :keywords
-  '("languages" "lisp" "slime")
-  :url "https://github.com/slime/slime")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
