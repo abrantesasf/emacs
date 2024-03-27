8801fe39afb0397c316888076e6f9e32eb2115e1
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-03-26 21:14:31 -0300

    Atualizações

---
+++ elpa/macrostep-20240325.2257/macrostep-pkg.el
@@ -0,0 +1,14 @@
+(define-package "macrostep" "20240325.2257" "Interactive macro expander"
+  '((cl-lib "0.5"))
+  :commit "edcab80c7f93baa9a6b5198a7fa47d03ece49114" :authors
+  '(("Jon Oddie" . "j.j.oddie@gmail.com"))
+  :maintainers
+  '(("Jon Oddie" . "j.j.oddie@gmail.com"))
+  :maintainer
+  '("Jon Oddie" . "j.j.oddie@gmail.com")
+  :keywords
+  '("lisp" "languages" "macro" "debugging")
+  :url "https://github.com/emacsorphanage/macrostep")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
