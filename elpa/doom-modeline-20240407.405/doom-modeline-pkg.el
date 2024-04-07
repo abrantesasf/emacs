f6942f1a08b750c83f6d3f8ff7a3877dd6484ebb
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2024-04-07 06:06:10 -0300

    Atualizações

---
+++ elpa/doom-modeline-20240407.405/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20240407.405" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.4.2")
+    (nerd-icons "0.1.0")
+    (shrink-path "0.3.1"))
+  :commit "60f8b34af946acbb4b2d64d600c47691fb525fff" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
