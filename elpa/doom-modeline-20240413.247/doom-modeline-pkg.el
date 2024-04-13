8f55d9f01c5dbc0a4efcc961a605e8f378e525dd
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-04-13 10:15:56 -0300

    Atualizações

---
+++ elpa/doom-modeline-20240413.247/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20240413.247" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.4.2")
+    (nerd-icons "0.1.0")
+    (shrink-path "0.3.1"))
+  :commit "1d0942ddd9aec2372ef783f22571a8ae6ddffcbd" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/slime-20240411.1436/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20240411.1436" "Superior Lisp Interaction Mode for Emacs"
+  '((emacs "24.3")
+    (macrostep "0.9"))
+  :commit "3247e0d8e6bb94b80f02d30e6ec9629270eaaa3f" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
