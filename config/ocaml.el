39745ff57210d6bc3c778aa811212a3fc71fcfaa
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2024-05-07 23:27:44 -0300

    configuração do OCaml e dot_vimrc

---
+++ config/ocaml.el
@@ -0,0 +1,5 @@
+;;----------------------------------------------------------;;
+;; Configuração para o OCaml:
+;;----------------------------------------------------------;;
+(add-to-list 'load-path "/home/computacao/.opam/default/share/emacs/site-lisp")
+(require 'ocp-indent)
