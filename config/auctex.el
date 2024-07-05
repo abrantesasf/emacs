65165430ed6ecfcc77b85b0df8146169390bddeb
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2024-07-05 00:27:25 -0300

    Atualizações

---
+++ config/auctex.el
@@ -0,0 +1,7 @@
+; Configurações para fazer AucTeX + RefTex:
+
+;; Turn on RefTeX in AUCTeX
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+
+;; Activate nice interface between RefTeX and AUCTeX
+(setq reftex-plug-into-AUCTeX t)
