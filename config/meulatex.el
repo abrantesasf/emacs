79667635113e0c50f7d89aba24f4d83038871c70
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2024-10-09 08:28:22 -0300

    Atualizações

--- config/meulatex.el
+++ config/meulatex.el
@@ -2,7 +2,7 @@
 
 
 ;; Turn on RefTeX in AUCTeX
-;(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
 ;; Activate nice interface between RefTeX and AUCTeX
-;(setq reftex-plug-into-AUCTeX t)
+(setq reftex-plug-into-AUCTeX t)
