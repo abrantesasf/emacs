3be18dccf215ff894748596ee375050063548db1
Abrantes Araújo Silva Filho <abrantesasf@pm.me>
2024-12-12 13:33:50 -0300

    Atualizações

--- config/arduino.el
+++ config/arduino.el
@@ -1,52 +1,14 @@
 ;;----------------------------------------------------------;;
 ;; Configurações para o Arduino
 ;;----------------------------------------------------------;;
-;;
-;; É necessário ter a IDE do Arduino (arduino.cc) instalada;
-;; É necessário ter o arduino-cli (github.com/arduino/arduino-cli) instalado;
 
-;; Major mode
-(use-package arduino-mode
-  :ensure t
-  :mode ("\\.ino\\'" . arduino-mode))
+(add-to-list 'auto-mode-alist '("\\.ino\\'" . c++-mode))
 
-;; Minor mode
-(use-package arduino-cli-mode
-  :ensure t
-  ;; :hook arduino-mode
-  ;; :mode "\\.ino\\'"
-  :custom
-  (arduino-cli-warnings 'all)
-  (arduino-cli-verify t))
-
-(add-to-list 'auto-minor-mode-alist '("\\.ino\\'" . arduino-cli-mode))
-
-;; Autocomplete
-(use-package company
-  :ensure t
-  :hook (arduino-mode . company-mode))
-
-(add-hook 'arduino-mode-hook 'company-mode)
-
-(add-hook 'arduino-mode-hook
+(add-hook 'c++-mode-hook
   (lambda ()
-    (setq-local company-backends '((company-dabbrev-code company-keywords company-files)))))
+    (c-set-style "stroustrup")
+    (setq c-basic-offset 2)
+    (electric-pair-mode 1)     ;; Adicionar parênteses e aspas automaticamente
+    (electric-indent-mode 1))) ;; Auto-indentar
 
-;(use-package company-clang
-;  :ensure t
-;  :config
-;  (setq company-clang-executable "/usr/bin/clang") ;; Verifique o caminho correto para o clang
-;  (add-hook 'arduino-mode-hook
-;            (lambda ()
-;              (setq-local company-backends '(company-clang company-dabbrev-code company-keywords)))))
-
-;(add-hook 'arduino-mode-hook
-;          (lambda ()
-;            (setq-local company-clang-arguments
-;                        '("-I/home/abrantesasf/.arduino15/libraries"
-;                          "-I/home/abrantesasf/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino"))))
-
-;; Syntax highligth
-(use-package flycheck
-  :ensure t
-  :hook (arduino-mode . flycheck-mode))
+(load-file "arduino_highlighting_code.el")
