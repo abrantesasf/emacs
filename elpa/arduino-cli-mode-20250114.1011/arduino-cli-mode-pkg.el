b28153869ca7716055c5868c7f5b5cacca09fcd2
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2025-02-11 01:04:15 -0300

    Atualizações

--- elpa/arduino-cli-mode-20250114.1011/arduino-cli-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "arduino-cli-mode" "20250114.1011"
-  "Arduino-CLI command wrapper."
-  '((emacs "25.1"))
-  :url "https://github.com/motform/arduino-cli-mode"
-  :commit "f4a26fcd9f0d4909a0f1e96db91b67a0a66bf2e5"
-  :revdesc "f4a26fcd9f0d"
-  :keywords '("processes" "tools"))
