b894df1082a7a28ce265e58bb7ecc03cadc440e4
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2025-03-28 17:41:33 -0300

    Atualizações

--- config/company_config.el
+++ config/company_config.el
@@ -35,5 +35,6 @@
 				"-I/usr/include/freetype2" "-I/usr/include/libpng16"
 				"-I/usr/include/gdk-pixbuf-2.0" "-I/usr/include/libmount"
 				"-I/usr/include/blkid" "-I/usr/include/glib-2.0"
-				"-I/usr/lib/x86_64-linux-gnu/glib-2.0/include"))
+				"-I/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+                                "-I/usr/include/x86_64-linux-gnu/sys"))
 
