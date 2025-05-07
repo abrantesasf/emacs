16f71a6dc56c4fbc13a833c7c5a580436c5918c0
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2025-05-07 10:14:40 -0300

    Atualizações

---
+++ elpa/yasnippet-snippets-20250507.834/yasnippet-snippets-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "yasnippet-snippets" "20250507.834"
+  "Collection of yasnippet snippets."
+  '((yasnippet "0.8.0"))
+  :url "https://github.com/AndreaCrotti/yasnippet-snippets"
+  :commit "aa5f9b98ba6ff8fa614b3e553cb38a6bf3c1295c"
+  :revdesc "aa5f9b98ba6f"
+  :keywords '("snippets")
+  :authors '(("Andrea Crotti" . "andrea.crotti.0@gmail.com"))
+  :maintainers '(("Andrea Crotti" . "andrea.crotti.0@gmail.com")))
