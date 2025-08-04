7bd09c94809f523945a3a2a32b4a3af1fcbd3633
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2025-08-04 01:06:41 -0300

    Atualizações

---
+++ elpa/lsp-mode-20250803.1951/lsp-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lsp-mode" "20250803.1951"
+  "LSP mode."
+  '((emacs         "28.1")
+    (dash          "2.18.0")
+    (f             "0.20.0")
+    (ht            "2.3")
+    (spinner       "1.7.3")
+    (markdown-mode "2.3")
+    (lv            "0")
+    (eldoc         "1.11"))
+  :url "https://github.com/emacs-lsp/lsp-mode"
+  :commit "226df20f9902c16f12bf7aedc50d5f28c931582c"
+  :revdesc "226df20f9902"
+  :keywords '("languages"))
---
+++ elpa/slime-20250803.2140/slime-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "slime" "20250803.2140"
+  "Superior Lisp Interaction Mode for Emacs."
+  '((emacs     "24.3")
+    (macrostep "0.9"))
+  :url "https://github.com/slime/slime"
+  :commit "f12bec08fa43e02c3e15550d5e4a2652939768cf"
+  :revdesc "f12bec08fa43"
+  :keywords '("languages" "lisp" "slime"))
