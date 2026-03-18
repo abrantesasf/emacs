8e06f39736a3ea4b06d3e06c56b51e97452903fc
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2026-03-18 17:42:59 -0300

    Atualizações

---
+++ elpa/ess-20260314.1643/ess-pkg.el
@@ -0,0 +1,20 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ess" "20260314.1643"
+  "Emacs Speaks Statistics."
+  '((emacs "25.1"))
+  :url "https://ess.r-project.org/"
+  :commit "62cef9175ddb31236eeb426bb0479496b2952197"
+  :revdesc "62cef9175ddb"
+  :authors '(("David Smith" . "dsmith@stats.adelaide.edu.au")
+             ("A.J. Rossini" . "blindglobe@gmail.com")
+             ("Richard M. Heiberger" . "rmh@temple.edu")
+             ("Kurt Hornik" . "Kurt.Hornik@R-project.org")
+             ("Martin Maechler" . "maechler@stat.math.ethz.ch")
+             ("Rodney A. Sparapani" . "rsparapa@mcw.edu")
+             ("Stephen Eglen" . "stephen@gnu.org")
+             ("Sebastian P. Luque" . "spluque@gmail.com")
+             ("Henning Redestig" . "henning.red@googlemail.com")
+             ("Vitalie Spinu" . "spinuvit@gmail.com")
+             ("Lionel Henry" . "lionel.hry@gmail.com")
+             ("J. Alexander Branham" . "alex.branham@gmail.com"))
+  :maintainers '(("ESS Core Team" . "ESS-core@r-project.org")))
