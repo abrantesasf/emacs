bce5a341edddbbba96d439547ec99f1c3afc7edc
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2026-04-23 17:32:14 -0300

    Atualizações

---
+++ elpa/ess-20260423.1426/ess-pkg.el
@@ -0,0 +1,20 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ess" "20260423.1426"
+  "Emacs Speaks Statistics."
+  '((emacs "25.1"))
+  :url "https://ess.r-project.org/"
+  :commit "5a09992aa6276fc8626a41d01d58b1d85959edaf"
+  :revdesc "5a09992aa627"
+  :authors '(("David Smith" . "dsmith@stats.adelaide.edu.au")
+             ("A.J. Rossini" . "blindglobe@gmail.com")
+             ("Richard M. Heiberger" . "rmh@temple.edu")
+             ("Kurt Hornik" . "Kurt.Hornik@R-project.org")
+             ("Martin Maechler" . "maechler@stat.math.ethz.ch")
+             ("Rodney A. Sparapani" . "rsparapa@mcw.edu")
+             ("Stephen Eglen" . "stephen@gnu.org")
+             ("Sebastian P. Luque" . "spluque@gmail.com")
+             ("Henning Redestig" . "henning.red@googlemail.com")
+             ("Vitalie Spinu" . "spinuvit@gmail.com")
+             ("Lionel Henry" . "lionel.hry@gmail.com")
+             ("J. Alexander Branham" . "alex.branham@gmail.com"))
+  :maintainers '(("ESS Core Team" . "ESS-core@r-project.org")))
--- elpa/slime-20260416.12/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "slime" "20260416.12"
-  "Superior Lisp Interaction Mode for Emacs."
-  '((emacs     "24.3")
-    (macrostep "0.9"))
-  :url "https://github.com/slime/slime"
-  :commit "82c13fa34b3314583712e5282f8879b3e1731f35"
-  :revdesc "82c13fa34b33"
-  :keywords '("languages" "lisp" "slime"))
