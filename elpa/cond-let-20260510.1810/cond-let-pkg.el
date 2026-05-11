bef903fe1a372e2d7f7d80c72d38b7207270666a
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2026-05-11 01:56:45 -0300

    Atualizações

---
+++ elpa/cond-let-20260510.1810/cond-let-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cond-let" "20260510.1810"
+  "Additional and improved binding conditionals."
+  '((emacs "28.1"))
+  :url "https://github.com/tarsius/cond-let"
+  :commit "d87c49286fc442d8b305fdb2357c96eb2cd6c757"
+  :revdesc "d87c49286fc4"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.cond-let@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.cond-let@jonas.bernoulli.dev")))
