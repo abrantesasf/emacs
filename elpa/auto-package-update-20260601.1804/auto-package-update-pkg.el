b157438589cdb071073cebdfd49914c5652a40f4
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2026-06-10 15:36:46 -0300

    Atualizações

---
+++ elpa/auto-package-update-20260601.1804/auto-package-update-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "auto-package-update" "20260601.1804"
+  "Automatically update Emacs packages."
+  '((emacs "24.4")
+    (dash  "2.1.0"))
+  :url "https://github.com/rranelli/auto-package-update.el"
+  :commit "e966c6c95de1742d867250dc15b1c6bd570b6ea5"
+  :revdesc "e966c6c95de1"
+  :keywords '("package" "update"))
--- elpa/slime-20260518.1422/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "slime" "20260518.1422"
-  "Superior Lisp Interaction Mode for Emacs."
-  '((emacs     "24.3")
-    (macrostep "0.9"))
-  :url "https://github.com/slime/slime"
-  :commit "41317b78dda1531e48d002df89694fb7878ca34d"
-  :revdesc "41317b78dda1"
-  :keywords '("languages" "lisp" "slime"))
