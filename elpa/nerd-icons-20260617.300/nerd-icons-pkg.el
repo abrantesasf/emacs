a8a763a91489d4c48b5efb86202352d81124ba6c
Abrantes Araújo Silva Fillho <abrantesasf@pm.me>
2026-06-17 15:38:27 -0300

    Atualizações

---
+++ elpa/nerd-icons-20260617.300/nerd-icons-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "nerd-icons" "20260617.300"
+  "Emacs Nerd Font Icons Library."
+  '((emacs "25.1"))
+  :url "https://github.com/rainstormstudio/nerd-icons.el"
+  :commit "158c5c19f5e3fb5985c9887c5decde7162eeb1ce"
+  :revdesc "158c5c19f5e3"
+  :keywords '("lisp")
+  :authors '(("Hongyu Ding" . "rainstormstudio@yahoo.com")
+             ("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers '(("Hongyu Ding" . "rainstormstudio@yahoo.com")
+                 ("Vincent Zhang" . "seagle0128@gmail.com")))
--- elpa/slime-20260609.1552/slime-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "slime" "20260609.1552"
-  "Superior Lisp Interaction Mode for Emacs."
-  '((emacs     "24.3")
-    (macrostep "0.9"))
-  :url "https://github.com/slime/slime"
-  :commit "2b738afe69a573e3729423a78216e8befc8d0502"
-  :revdesc "2b738afe69a5"
-  :keywords '("languages" "lisp" "slime"))
